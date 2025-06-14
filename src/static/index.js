3be0b1da7dc9f738e3fd1e32a46d136f692ef29d
aKuad <53811809+aKuad@users.noreply.github.com>
2025-06-14 23:06:29 +0900

    Mod: `is_only_num_alp` return reason in code - description text moved to `index.js`
    Signed-off-by: aKuad <53811809+aKuad@users.noreply.github.com>

--- src/static/index.js
+++ src/static/index.js
@@ -9,18 +9,22 @@ import { random_str_gen } from "./util/random_str_gen.js";
 
 globalThis.addEventListener("load", () => {
   const page_lang = new URL(location.href).pathname.split("/")[1];
+  const incorrect_reason_en = {};
+  incorrect_reason_en[1] = "Non-ascii character including";
+  incorrect_reason_en[2] = "Ascii space including";
+  incorrect_reason_en[3] = "Ascii symbol including";
   const incorrect_reason_ja = {};
-  incorrect_reason_ja["Non-ascii character including"] = "半角英数字以外は使用できません";
-  incorrect_reason_ja["Ascii space including"] = "スペースは使用できません";
-  incorrect_reason_ja["Ascii symbol including"] = "記号は使用できません";
+  incorrect_reason_ja[1] = "半角英数字以外は使用できません";
+  incorrect_reason_ja[2] = "スペースは使用できません";
+  incorrect_reason_ja[3] = "記号は使用できません";
 
   document.getElementById("room-id-input").placeholder = random_str_gen(10);
 
   document.getElementById("room-id-input").addEventListener("input", e => {
     const room_id = e.target.value || e.target.placeholder;
 
-    const {is_correct, incorrect_reason} = is_only_num_alp(room_id);
-    if(is_correct) {
+    const id_check_result = is_only_num_alp(room_id);
+    if(id_check_result === 0) {
       document.getElementById("join-participant").href = `/${page_lang}/participant/${room_id}`;
       document.getElementById("join-moderator").href = `/${page_lang}/moderator/${room_id}`;
       document.getElementById("join-participant").classList.remove("link-disabled");
@@ -31,7 +35,7 @@ globalThis.addEventListener("load", () => {
       document.getElementById("join-moderator").href = "";
       document.getElementById("join-participant").classList.add("link-disabled");
       document.getElementById("join-moderator").classList.add("link-disabled");
-      document.getElementById("room-id-error").innerText = page_lang === "ja" ? incorrect_reason_ja[incorrect_reason] : incorrect_reason;
+      document.getElementById("room-id-error").innerText = page_lang === "ja" ? incorrect_reason_ja[id_check_result] : incorrect_reason_en[id_check_result];
     }
   });
 
--- src/tests/js_denotest/is_only_num_alp.test.js
+++ src/tests/js_denotest/is_only_num_alp.test.js
@@ -26,10 +26,10 @@ Deno.test(async function true_cases(t) {
     const lower_alphabets   = "az";
     const numbers_alphabets = "09azAZ";
 
-    assertEquals(is_only_num_alp(numbers          ).is_correct, true);
-    assertEquals(is_only_num_alp(upper_alphabets  ).is_correct, true);
-    assertEquals(is_only_num_alp(lower_alphabets  ).is_correct, true);
-    assertEquals(is_only_num_alp(numbers_alphabets).is_correct, true);
+    assertEquals(is_only_num_alp(numbers          ), 0);
+    assertEquals(is_only_num_alp(upper_alphabets  ), 0);
+    assertEquals(is_only_num_alp(lower_alphabets  ), 0);
+    assertEquals(is_only_num_alp(numbers_alphabets), 0);
   });
 
 
@@ -43,19 +43,8 @@ Deno.test(async function true_cases(t) {
     const ascii_space  = " ";
     const ascii_symbol = "%";
 
-    const non_ascii_res    = is_only_num_alp(non_ascii);
-    const ascii_space_res  = is_only_num_alp(ascii_space);
-    const ascii_symbol_res = is_only_num_alp(ascii_symbol);
-
-    console.log(non_ascii_res);
-    console.log(ascii_space_res);
-    console.log(ascii_symbol_res);
-
-    assertEquals(non_ascii_res.is_correct   , false);
-    assertEquals(ascii_space_res.is_correct , false);
-    assertEquals(ascii_symbol_res.is_correct, false);
-    assertEquals(non_ascii_res.incorrect_reason   , "Non-ascii character including");
-    assertEquals(ascii_space_res.incorrect_reason , "Ascii space including");
-    assertEquals(ascii_symbol_res.incorrect_reason, "Ascii symbol including");
+    assertEquals(is_only_num_alp(non_ascii), 1);
+    assertEquals(is_only_num_alp(ascii_space), 2);
+    assertEquals(is_only_num_alp(ascii_symbol), 3);
   });
 });
