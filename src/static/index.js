b3c4e44be3b05959656e46dca38f81e0a35e5504
aKuad <53811809+aKuad@users.noreply.github.com>
2025-06-19 22:17:03 +0900

    Mod: Join link on index relative path - this way is more simple
    Signed-off-by: aKuad <53811809+aKuad@users.noreply.github.com>

--- src/static/index.js
+++ src/static/index.js
@@ -8,8 +8,6 @@ import { is_only_num_alp } from "./util/is_only_num_alp.js";
 import { random_str_gen } from "./util/random_str_gen.js";
 
 globalThis.addEventListener("load", () => {
-  const page_lang = new URL(location.href).pathname.split("/")[1];
-
   document.getElementById("room-id-input").placeholder = random_str_gen(10);
 
   document.getElementById("room-id-input").addEventListener("input", e => {
@@ -17,8 +15,8 @@ globalThis.addEventListener("load", () => {
 
     const id_check_result = is_only_num_alp(room_id);
     if(id_check_result === 0) {
-      document.getElementById("join-participant").href = `/${page_lang}/participant/${room_id}`;
-      document.getElementById("join-moderator").href = `/${page_lang}/moderator/${room_id}`;
+      document.getElementById("join-participant").href = `./participant/${room_id}`;
+      document.getElementById("join-moderator").href = `./moderator/${room_id}`;
       document.getElementById("join-participant").classList.remove("link-disabled");
       document.getElementById("join-moderator").classList.remove("link-disabled");
       document.getElementsByClassName("room-id-error-select")[0].checked = true;
