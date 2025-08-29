e6363bf5bebdb4e781df36fba91c77150f3e97dd
aKuad <53811809+aKuad@users.noreply.github.com>
2025-08-29 22:43:17 +0900

    Add: Last input room-id store/restore feature at `index.js`
    Signed-off-by: aKuad <53811809+aKuad@users.noreply.github.com>

--- src/static/index.js
+++ src/static/index.js
@@ -8,8 +8,11 @@ import { is_only_num_alp } from "./util/is_only_num_alp.js";
 import { random_str_gen } from "./util/random_str_gen.js";
 
 globalThis.addEventListener("load", () => {
+  // Room ID restore & Default room ID set
   document.getElementById("room-id-input").placeholder = random_str_gen(10);
+  document.getElementById("room-id-input").value = localStorage.getItem("last-input-room-id") || "";
 
+  // Room ID input process
   document.getElementById("room-id-input").addEventListener("input", e => {
     const room_id = e.target.value || e.target.placeholder;
 
@@ -28,6 +31,17 @@ globalThis.addEventListener("load", () => {
       document.getElementsByClassName("room-id-error-select")[id_check_result].checked = true;
     }
   });
+
+  // Room ID store
+  function on_room_id_entered() {
+    const room_id_input = document.getElementById("room-id-input").value;
+    if(room_id_input)
+      localStorage.setItem("last-input-room-id", room_id_input);
+    else
+      localStorage.removeItem("last-input-room-id");
+  }
+  document.getElementById("join-participant").addEventListener("click", on_room_id_entered);
+  document.getElementById("join-moderator").addEventListener("click", on_room_id_entered);
 
   // For initialize join link
   document.getElementById("room-id-input").dispatchEvent(new Event("input"));
